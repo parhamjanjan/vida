29219880de77b44b9dd3c2a58679c51711618911
parhamjanjan <139699315+parhamjanjan@users.noreply.github.com>
2026-06-15 12:11:28 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./**/*.html",
+    "./public_exam/**/*.html",
+    "./planner/**/*.html",
+    "./premade_exam/**/*.html",
+    "./exam/**/*.html",
+    "./study_ai/**/*.html",
+    "./anti-sleeping/**/*.html",
+    "./notebook/**/*.html",
+    "./login/**/*.html"
+
+    // مسیر فایل‌های پروژه خود را اضافه کنید
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
